e751fc3a33727b90f65fd8a1afd9c79e31d77fb2
Padmapriya <padmapriyajyoti99@gmail.com>
2023-01-25 23:10:15 +0530

    New Class added

---
+++ src/com/velocity/git/AscendingOrder.java
@@ -0,0 +1,16 @@
+package com.velocity.git;
+
+import java.util.Vector;
+
+public class AscendingOrder {
+
+	public static void main(String[] args) {
+		int [] a = {1,4,8,2,3,5,9};
+		Vector<Integer> v = new Vector<Integer>();
+		for(int b :a) {
+			System.out.println(b);
+		}		
+		
+	}
+
+}
