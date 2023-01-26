f277fb315010687dab2a2fab385ae5833bd47fbc
Padmapriya <padmapriyajyoti99@gmail.com>
2023-01-26 09:48:14 +0530

    Nos Modified

--- src/com/velocity/git/List.java
+++ src/com/velocity/git/List.java
@@ -8,11 +8,11 @@ public class List {
 	public static void main(String[] args) {
 		LinkedList<Integer> list = new LinkedList<Integer>();
 		list.add(12);
-		list.add(11);
-		list.add(14);
 		list.add(13);
-		list.add(16);
+		list.add(14);
 		list.add(15);
+		list.add(16);
+		list.add(17);
 		Iterator<Integer> itr = list.iterator();
 		while(itr.hasNext()) {
 			 Integer next =itr.next();
